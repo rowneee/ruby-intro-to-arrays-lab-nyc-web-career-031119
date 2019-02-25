3fae48821b891db922884350e8371e527271e693
Roni Shabo <roni.shabo@gmail.com>
2019-02-25 20:39:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -5,3 +5,16 @@ end
 def array_with_two_elements
   return array = ["one", "two"]
 end
+
+def first_element(array)
+  return array[0]
+end
+
+def third_element(array)
+  return array[2]
+end
+
+def last_element(array)
+  return array[-1]
+end
+
